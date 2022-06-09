bc78f44ed06fe647935bed32ed34f4076a31507d
ngacute4180 <107206779+ngacute4180@users.noreply.github.com>
2022-06-09 23:44:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "vào đây rồi thì ibb dii bạn chẻ ",
+    introTitle: 'iuu',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
